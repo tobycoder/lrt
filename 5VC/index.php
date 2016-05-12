ca1212700720131300ba491ebebd19f38ec4dcf2
Floris de Boer <florisblog.com@gmail.com>
2016-05-12 23:19:57 +0200

    made repo complete

---
+++ 5VC/index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE  HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"  "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+  <head>
+      <link rel="stylesheet" href="style.css" type="text/css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <meta  http-equiv="Content-Type" content="text/html;  charset=iso-8859-1">
+    <title>Je maatje zoeken</title>
+  </head>
+  <p><body>
+    <h1>Vind je maatje en je boekje voor 5VC</h1>
+    <p>Kut he dat mondeling. Gelukkig kan dit je tijd besparen. </p>
+      <div class="alert alert-warning" role="alert"><strong>Let op!</strong> Als 1 of meer van jouw boeken niet hierbij staat, is de kans groot dat je hem niet hoeft voor te bereiden.</div>
+    <a href="insert.php" class="btn btn-info">Voeg je boeken toe aan de database</a><br><br>
+    <form  method="post" action="index.php?go"  id="searchform">
+      <input  type="text" name="name">
+      <input  type="submit" name="submit" class="btn btn-success" value="ZOEKEN!1!11!">
+    </form>
+  </body></p>
+</html>
+<?php
+  if(isset($_POST['submit'])){
+  if(isset($_GET['go'])){
+  $query=$_POST['name'];
+  //connect  to the database
+  $db=mysql_connect("db.florisdeboer.com", "md322726db343701",  "1me1loMM") or die ('I cannot connect to the database  because: ' . mysql_error());
+  //-select  the database to use
+  $mydb=mysql_select_db("md322726db343701");
+  //-query  the database table
+  $sql="SELECT  Naam, Boek1, Boek2, Boek3, Boek4 FROM boekjes2 WHERE Boek1 LIKE '%" . $query .  "%' OR Boek2 LIKE '%" . $query ."%' OR Boek3 LIKE '%" . $query .  "%' OR Boek4 LIKE '%" . $query .  "%'";
+  //-run  the query against the mysql query function
+  $result=mysql_query($sql);
+
+      if(!$result){
+          echo "<div class='alert alert-danger'>Er is een error opgetreden. Bel of WhatsApp me: 0637609345</div>";
+      }
+      
+    echo "    <table class='table'>
+        <thead>
+            <tr>
+                <th>Naam</th>
+                <th>Boek 1</th>
+                <th>Boek 2</th>
+                <th>Boek 3</th>
+                <th>Boek 4</th>
+            </tr>
+        </thead>";
+  //-create  while loop and loop through result set
+  while($row=mysql_fetch_array($result)){
+          $Name=$row['Naam'];
+          $Boek1=$row['Boek1'];
+          $Boek2=$row['Boek2'];
+          $Boek3=$row['Boek3'];
+          $Boek4=$row['Boek4'];
+      
+    echo "<tbody>
+        <tr>
+            <th scope='row'>".$Name."</th>
+            <td>".$Boek1."</td>
+            <td>".$Boek2."</td>
+            <td>".$Boek3."</td>
+            <td>".$Boek4."</td>
+        </tr>
+    </tbody>";
+    
+  }
+      
+      echo "</table>";
+
+  }
+  }
+  else{
+  echo  "<p>Zoek bijvoorbeeld op: 'M.' </p>";
+  }
+?>
+
---
+++ 5VD/insert.php
@@ -0,0 +1,62 @@
+<!DOCTYPE  HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"  "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+  <head>
+      <link rel="stylesheet" href="style.css" type="text/css">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <meta  http-equiv="Content-Type" content="text/html;  charset=iso-8859-1">
+    <title>Toevoegen</title>
+  </head>
+  <p><body>
+    <h1>Boeken toevoegen</h1>
+    <p>Je mondeling. Jup schijt aan die. Gelukkig hebben we een duo-toets en kan je, door hier je boeken toe te voegen, kijken met wie je deze verschrikking kan voltooien. Veel plezier!</p>
+    <a href="http://www.florisdeboer.com/luxor/5VD" class="btn btn-info">Zuruck gehen</a><br><br>
+<form  method="post" action="insert.php"  id="searchform">
+      <table class="table">
+        <thead>
+            <tr>
+                <th>Naam:</th>
+                <th>Boek 1:</th>
+                <th>Boek 2:</th>
+                <th>Boek 3:</th>
+                <th>Boek 4:</th>
+            </tr>
+          </thead>
+          <tbody>
+              <tr>
+                  <td><input  type="text" name="name"><br></td>
+                  <td><input  type="text" name="boek1"><br></td>
+                  <td><input  type="text" name="boek2"><br></td>
+                  <td><input  type="text" name="boek3"><br></td>
+                  <td><input  type="text" name="boek4"><br></td>
+              </tr>
+          </tbody>
+          
+    
+      </table><br>
+    <input  type="submit" name="submit" value="TOEVOEGEN!!11!1" class="btn btn-success">
+      </form>
+  </body></p>
+</html>
+<?php
+  if(isset($_POST['submit'])){
+      $naam = $_POST['name'];
+      $boek1 = $_POST['boek1'];
+      $boek2 = $_POST['boek2'];
+      $boek3 = $_POST['boek3'];
+      $boek4 = $_POST['boek4'];
+
+  //connect  to the database
+  $db=mysql_connect("db.florisdeboer.com", "md322726db343701",  "1me1loMM") or die ('Ik ken niet connecten: ' . mysql_error());
+  //-select  the database to use
+  $mydb=mysql_select_db("md322726db343701");
+  //-query  the database table
+  $sql="INSERT INTO boekjes3 (Naam, Boek1, Boek2, Boek3, Boek4) VALUES ('$naam', '$boek1', '$boek2', '$boek3', '$boek4')";
+  //-run  the query against the mysql query function
+  $result=mysql_query($sql);
+      echo "<div class='alert alert-success'>Yes! gelukt!</div>";
+      
+  }
+?>
